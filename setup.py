f06d51425a400105543528efc32e0fad895f32ee
inductive-anks1 <ankit.kumar@plaksha.edu.in>
2025-01-10 12:35:30 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building ML Model that can predict the Food Delivery Time in Minutes.',
+    author='Ankit Kuma',
+    license='MIT',
+)
